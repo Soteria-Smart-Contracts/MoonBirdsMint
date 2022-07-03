66420b0825a9eecf02b8a207c8a011f18135e6d9
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-03 17:33:39 -0400

    Add Wallet Section

---
+++ Website/src/Functionality.js
@@ -0,0 +1,34 @@
+SwitchPage("mint");
+
+
+
+
+function SwitchPage(page) {
+    if(page == "mint"){
+    document.getElementById("wallet").style.display = "none";
+    document.getElementById("minting").style.display = "block";
+    }
+    if(page == "wallet"){
+    document.getElementById("minting").style.display = "none";
+    document.getElementById("wallet").style.display = "block";
+    }
+
+}
+
+
+function LoadNFTs() {
+    let index = 0;
+    while(index < ids.length){
+        newcard = document.createElement("div");
+        newcard.className = "NFTcard";
+        newcard.id = index;
+        let img = '<img class="nftimage" src="src/images/collection/' + ids[index] + '.png"></img>"';
+        let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + ids[index] +  '/token-transfers';
+        newcard.innerHTML = img + '<a class="nftID head">MoonBird ID:</a>' + '<br>' + '<a class="nftID">' + ids[index] + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
+        document.getElementById("WalletContainer").appendChild(newcard);
+        index++;
+    }
+
+}
+
+console.log(document.getElementById("DefaultCard").innerHTML);
--- Website/src/Web3.js
+++ Website/src/Web3.js
@@ -1,10 +1,10 @@
 let account;
 const ABI = window.abi;
+let ids;
 let netID;
 const contractAddress = "0x2001D679210C0e4531f5C07155d8e3677Dd388ae";
 
 loginWithEth();
-
 
 async function loginWithEth(){
     if(window.ethereum){
@@ -18,9 +18,11 @@ async function loginWithEth(){
             return("Failed to connect")
         }
         accountarray = await web3.eth.getAccounts();
-        contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
+        contract = await new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
         getsupply();
+        await ReturnIds();
+        LoadNFTs();
     } else { 
         alert("No ETHER Wallet available")
     }
@@ -44,4 +46,10 @@ async function mint(){
     let tx = await contract.methods.mint(amount).send({from: account, value: amountWei, gas: 3000000});
     console.log(tx);
     return(tx);
+}
+
+async function ReturnIds(){
+    ids = await contract.methods.walletOfOwner(account).call();
+    console.log(ids);
+    return(ids);
 }
