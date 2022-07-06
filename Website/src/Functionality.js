0bb00fb62dcff7a3890d1bf1e89e044c86fefb32
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-06 10:17:05 -0400

    July 6, 2022 10:17 AM

--- Website/src/Functionality.js
+++ Website/src/Functionality.js
@@ -2,12 +2,12 @@ SwitchPage("mint");
 
 
 function ShowCollection(){
-    let index = 0;
+    let index = 1;
     while(index < 100){
         newcard = document.createElement("div");
         newcard.className = "NFTcard";
         newcard.id = index;
-        let img = '<img class="nftimage" src="src/images/collection/' + ids[index] + '.png"></img>"';
+        let img = '<img class="nftimage" src="src/images/collection/' + index + '.png"></img>"';
         let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + ids[index] +  '/token-transfers';
         newcard.innerHTML = img + '<a class="nftID head">MoonBird ID:</a>' + '<br>' + '<a class="nftID">' + ids[index] + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
         document.getElementById("WalletContainer").appendChild(newcard);
