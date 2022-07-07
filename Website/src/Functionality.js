c50b2ba55d36c997bdae2c4fc08e6109fb15d02c
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-07 11:38:54 -0400

    July 7, 2022 11:38 AM

--- Website/src/Functionality.js
+++ Website/src/Functionality.js
@@ -2,7 +2,7 @@ SwitchPage("mint");
 let CollectionNum = 10000;
 let updown;
 
-document.getElementById("txviewer").textContent = "";
+document.getElementById("txviewer").style.display = "none";
 
 function setUpdown(direction){
     document.getElementById("CollectionContainer").innerHTML = "";
