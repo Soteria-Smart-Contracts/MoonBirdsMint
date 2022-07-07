86915ec1c4cdd7200114275a3c699e7e7b796c25
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-07 12:58:04 -0400

    July 7, 2022 12:58 PM

--- Website/src/Web3.js
+++ Website/src/Web3.js
@@ -43,6 +43,9 @@ async function getID(){
 async function mint(){
     let amount = document.getElementById("amount").value;
     let txfailed = false;
+    document.getElementById("txviewer").textContent = "Transaction Complete";
+    document.getElementById("txviewer").target = "_blank";
+    document.getElementById("txviewer").href = "https://blockscout.com/etc/mainnet/tx/" + tx.transactionHash;
     if(amount < 1){ 
         alert("Please enter a valid amount");
         return;
