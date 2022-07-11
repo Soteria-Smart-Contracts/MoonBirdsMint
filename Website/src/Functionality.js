4f086828a96682e10d2c1c591543a183e6b6bb46
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-11 13:03:41 -0400

    July 11, 2022 1:03 PM

--- Website/src/Functionality.js
+++ Website/src/Functionality.js
@@ -21,7 +21,7 @@ function ShowCollection(){
     let index = CollectionNum;
 
     if (updown == "down"){
-        index = CollectionNum
+        index = CollectionNum - 1;
         while(index > (CollectionNum - 100)){
             if (index < 1 || index > 10000){
                 break;
