023cc077c7eb38b163b889da7653be22adfa58b4
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-11 12:59:17 -0400

    July 11, 2022 12:59 PM

--- Website/src/Functionality.js
+++ Website/src/Functionality.js
@@ -41,6 +41,7 @@ function ShowCollection(){
             if (index < 1 || index > 10000){
                 break;
             }
+            index
             newcard = document.createElement("div");
             newcard.className = "NFTcard";
             newcard.id = index;
