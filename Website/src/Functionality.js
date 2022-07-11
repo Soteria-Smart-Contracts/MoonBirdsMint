48d52c032856cc232721f3c60430f96560d738fe
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-07-11 12:59:54 -0400

    July 11, 2022 12:59 PM

--- Website/src/Functionality.js
+++ Website/src/Functionality.js
@@ -44,10 +44,10 @@ function ShowCollection(){
             let indexid = window.rankingorder[index];
             newcard = document.createElement("div");
             newcard.className = "NFTcard";
-            newcard.id = index;
-            let img = '<img class="nftimage" src="src/images/collection/' + index + '.png"></img>"';
-            let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + index +  '/token-transfers';
-            newcard.innerHTML = img + '<a class="nftID head">MoonBird ID:</a>' + '<br>' + '<a class="nftID">' + index + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
+            newcard.id = indexid;
+            let img = '<img class="nftimage" src="src/images/collection/' + indexid + '.png"></img>"';
+            let BlockscoutLink = 'https://blockscout.com/etc/mainnet/token/0x2001d679210c0e4531f5c07155d8e3677dd388ae/instance/' + indexid +  '/token-transfers';
+            newcard.innerHTML = img + '<a class="nftID head">MoonBird ID:</a>' + '<br>' + '<a class="nftID">' + indexid + '</a>' + '<br>' + '<a class="nftID scout" href="' + BlockscoutLink + '" target="_blank">View On Blockscout</a>';
             document.getElementById("CollectionContainer").appendChild(newcard);
             index++;
         }
